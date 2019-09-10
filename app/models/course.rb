132ba93253bfd4dc4c0696454e8213686578c5cd
Rebecca Douglass <beccadouglass@yahoo.com>
2019-09-10 21:11:18 +0000

    reorder sections

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,7 @@ class Course < ApplicationRecord
     mount_uploader :image, ImageUploader
     has_many :enrollments
 
+
     validates :title, presence: true
     validates :description, presence: true
     validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}

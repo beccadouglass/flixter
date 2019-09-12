cd2f031fe5057f6a38b5cc4123c705aa02ec3bae
Rebecca Douglass <beccadouglass@yahoo.com>
2019-09-12 21:50:47 +0000

    section add modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,11 +1,8 @@
 class Instructor::SectionsController < ApplicationController
     before_action :authenticate_user!
-    before_action :require_authorized_for_current_course, only: [:new, :create]
+    before_action :require_authorized_for_current_course, only: [:create]
     before_action :require_authorized_for_current_section, only: [:update]
     
-    def new
-        @section = Section.new
-    end
 
     def create
         @section = current_course.sections.create(section_params)
@@ -24,7 +21,7 @@ class Instructor::SectionsController < ApplicationController
             render plain: 'Unauthorized', status: :unauthorized
         end
     end
-    
+
     def current_section
         @current_section ||= Section.find(params[:id])
     end

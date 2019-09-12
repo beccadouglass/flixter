92a6ac9f75ff7935e9bcbc86d8ee8f4b8335f5e4
Rebecca Douglass <beccadouglass@yahoo.com>
2019-09-12 18:43:40 +0000

    working on errors

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :lessons, only: [:update]
-    resources :sections, only: [:update]
+    resources :lessons, only: [:update] do
     resources :lessons, only: [:new, :create]
+    end
       
 
     resources :courses, only: [:new, :create, :show] do
